18ff3bdb18d9d787ad097d7947250234d60b1379
Kamil <kamil.adamiec99@gmail.com>
2022-09-27 18:10:58 +0200

    Register and login system

---
+++ Assets/Scripts/PlayFabManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayFabManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI information;
+    public TMP_InputField emailInput;
+    public TMP_InputField passwordInput;
+    public Button loginButton;
+    public Button registerButton;
+    public Button passwordResetButton;
+
+    public void RegisterButton()
+    {
+        if(emailInput.text == "")
+        {
+            information.text = "The email address is empty!";
+            return;
+        }
+        else if(passwordInput.text.Length < 6)
+        {
+            information.text = "The password is too short!";
+            return;
+        }
+        else 
+        {
+            var request = new RegisterPlayFabUserRequest {
+            Email = emailInput.text,
+            Password = passwordInput.text,
+            RequireBothUsernameAndEmail = false
+            };
+            PlayFabClientAPI.RegisterPlayFabUser(request, OnRegisterSuccess, OnError);
+        }
+    }
+
+    public void OnRegisterSuccess(RegisterPlayFabUserResult result)
+    {
+        information.text = "Registered and logged in";
+    }
+
+    public void LoginButton()
+    {
+        if(emailInput.text == "")
+        {
+            information.text = "The email address is empty!";
+            return;
+        }
+        else if(passwordInput.text == "")
+        {
+            information.text = "The password is empty!";
+            return;
+        }
+        else
+        {
+            var request = new LoginWithEmailAddressRequest {
+                Email = emailInput.text,
+                Password = passwordInput.text
+            };
+            PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnError);
+        }
+    }
+
+    public void OnLoginSuccess(LoginResult resault)
+    {
+        information.text = "Logged in";
+    }
+    
+    void Start()
+    {
+        //Login();
+        registerButton.onClick.AddListener(() => { RegisterButton();});
+        loginButton.onClick.AddListener(() => { LoginButton();});
+    }
+    
+
+
+    // void Login()
+    // {
+    //     var request = new LoginWithCustomIDRequest {
+    //         CustomId = SystemInfo.deviceUniqueIdentifier,
+    //         CreateAccount = true
+    //     };
+    //     PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+    // }
+
+    void OnSuccess(LoginResult result)
+    {
+        Debug.Log("Successful login/account create!");
+    }
+
+    void OnError(PlayFabError error)
+    {
+        information.text = error.ErrorMessage;
+        Debug.Log(error.GenerateErrorReport());
+    }
+}

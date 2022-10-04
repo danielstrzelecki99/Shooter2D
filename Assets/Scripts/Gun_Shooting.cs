f0b4104c468ddc73a06da2bf73d5872ec476df2d
AdrianSzymczyk <adiszymczyk@gmail.com>
2022-10-04 18:10:03 +0200

    Shooting animation

--- Assets/Scripts/Gun_Shooting.cs
+++ Assets/Scripts/Gun_Shooting.cs
@@ -7,17 +7,30 @@ public class Gun_Shooting : MonoBehaviour
 
     public Transform fire_Point;
     public GameObject bulletPrefab;
+    private Animator animator;
+    private bool shot = false;
 
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            Shooot();
+            Shoot();
+            
+            Debug.Log(shot);
         }
+        else
+            shot = false;
+        animator.SetBool("shot", shot);
     }
-    void Shooot ()
+    void Shoot ()
     {
         Instantiate(bulletPrefab, fire_Point.position, fire_Point.rotation);
+        shot = true;
     }
 }

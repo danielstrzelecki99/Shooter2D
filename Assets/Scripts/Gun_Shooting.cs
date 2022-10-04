f2995e0be4995d8ccac001ccd1b384fb92732433
AdrianSzymczyk <adiszymczyk@gmail.com>
2022-10-04 23:11:46 +0200

    Shooting in line system

--- Assets/Scripts/Gun_Shooting.cs
+++ Assets/Scripts/Gun_Shooting.cs
@@ -6,9 +6,11 @@ public class Gun_Shooting : MonoBehaviour
 {
 
     public Transform fire_Point;
-    public GameObject bulletPrefab;
+    //public GameObject bulletPrefab;
+    public GameObject impactEffect;
     private Animator animator;
     private bool shot = false;
+    public LineRenderer lineRenderer;
 
 
     private void Awake()
@@ -20,17 +22,33 @@ public class Gun_Shooting : MonoBehaviour
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            Shoot();
-            
-            Debug.Log(shot);
+            StartCoroutine(Shoot());
         }
         else
             shot = false;
         animator.SetBool("shot", shot);
     }
-    void Shoot ()
+    IEnumerator Shoot ()
     {
-        Instantiate(bulletPrefab, fire_Point.position, fire_Point.rotation);
+        //Instantiate(bulletPrefab, fire_Point.position, fire_Point.rotation);
+        RaycastHit2D hitInfo = Physics2D.Raycast(fire_Point.position, fire_Point.right);
         shot = true;
+        if (hitInfo)
+        {
+            Debug.Log(hitInfo.transform.name);
+            Instantiate(impactEffect, hitInfo.point, Quaternion.identity);
+            lineRenderer.SetPosition(0, fire_Point.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+        }
+        else
+        {
+            lineRenderer.SetPosition(0, fire_Point.position);
+            lineRenderer.SetPosition(1, fire_Point.position + fire_Point.right * 100);
+        }
+
+        lineRenderer.enabled = true;
+        // waiting to disable line
+        yield return new WaitForSeconds(0.02f);
+        lineRenderer.enabled = false;
     }
 }

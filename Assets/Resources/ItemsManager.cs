30c6973e281363f2858da3ddb2b60a8ab2ca1b16
AdrianSzymczyk <adiszymczyk@gmail.com>
2022-10-28 23:10:44 +0200

    Added ammo icon, problems fixed

--- Assets/Resources/ItemsManager.cs
+++ Assets/Resources/ItemsManager.cs
@@ -1,9 +1,10 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
-public class ItemsManager : MonoBehaviour
+public class ItemsManager : MonoBehaviourPun
 {
     public static GameObject selectedObject;
     public static string itemInfoText;

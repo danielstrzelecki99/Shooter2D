f7a4fceab9f418537c945ebc92db770b0c4626a0
danielstrzelecki99 <danielstrzelecki99@wp.pl>
2022-11-07 14:38:13 +0100

    Add private rooms

--- Assets/Scripts/Rooms/RoomListing.cs
+++ Assets/Scripts/Rooms/RoomListing.cs
@@ -9,17 +9,33 @@ public class RoomListing : MonoBehaviour
 {
     [SerializeField]
     private Text _text;
+    private bool _isPrivate;
+    private CreateOrJoinCanvas createOrJoinCanvas;
+    public GameObject lockImage;
 
     public RoomInfo RoomInfo { get; private set; }
+
+    private void Awake()
+    {
+        createOrJoinCanvas = GetComponentInParent<CreateOrJoinCanvas>();
+    }
 
     public void setRoomInfo(RoomInfo roomInfo)
     {
         RoomInfo = roomInfo;
+        _isPrivate = (bool)roomInfo.CustomProperties["isPrivate"];
         _text.text = roomInfo.MaxPlayers + ", " + roomInfo.Name;
+        if(_isPrivate)
+            lockImage.SetActive(true);
     }
 
     public void OnClick_Button()
     {
-        PhotonNetwork.JoinRoom(RoomInfo.Name);
+        if (_isPrivate)
+        {
+            createOrJoinCanvas.EnterPasswordWindowShow();
+            createOrJoinCanvas.room = RoomInfo;
+        }
+        else PhotonNetwork.JoinRoom(RoomInfo.Name);
     }
 }

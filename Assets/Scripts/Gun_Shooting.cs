9432a02acdffdb73f80f7b58ee424207deac7ffc
AdrianSzymczyk <adiszymczyk@gmail.com>
2022-11-08 15:14:14 +0100

    Follow forearm after mouse cursor

--- Assets/Scripts/Gun_Shooting.cs
+++ Assets/Scripts/Gun_Shooting.cs
@@ -9,6 +9,7 @@ using Photon.Pun.Demo.Asteroids;
 public class Gun_Shooting : MonoBehaviourPun
 {
     public Transform gunHolder;
+    public Transform forearmHolder;
     public GameObject Bullet;
     float ReadyForNextShoot;
 
@@ -53,22 +54,10 @@ public class Gun_Shooting : MonoBehaviourPun
             AmaxClipSize = weaponController.maxClipSize;
             AcurrentAmmo = weaponController.currentAmmo;
             AmaxAmmoSize = weaponController.maxAmmoSize;
-            //rotate gun towards mousePosition
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - gunHolder.position;
-            float rotZ = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
-            gunHolder.transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
-            if (rotZ < 90 && rotZ > -89)
-            {
-                //Debug.Log("Facing right");
-                gunHolder.transform.Rotate(0f, 0f, gunHolder.transform.rotation.z);
-            }
-            else
-            {
-                //Debug.Log("Facing left");
-                gunHolder.transform.Rotate(180f, 0f, gunHolder.transform.rotation.z);
-            }
+            FlipWeapon(gunHolder, 0);
+            FlipWeapon(forearmHolder, WeaponManager.CurrentWeaponNo);
 
-            //activate animation fire button is pressed
+            //activate animation when fire button is pressed
             if (Input.GetMouseButton(0))
             {
                 if (Time.time > ReadyForNextShoot)
@@ -81,10 +70,12 @@ public class Gun_Shooting : MonoBehaviourPun
             else
                 shot = false;
             animator.SetBool("shoot", shot);
+            //reload weapon when R button is pressed
             if (Input.GetKeyDown(KeyCode.R))
             {
                 weaponController.Reload();
             }
+            //switch reference to weapon when C button is pressed
             if (Input.GetKeyDown(KeyCode.C))
             {
                 weaponController = weapon.GetComponent<WeaponScript>();
@@ -138,6 +129,26 @@ public class Gun_Shooting : MonoBehaviourPun
     //    weaponController.currentAmmo -= reloadAmount;
     //    Debug.Log($"{weaponController.currentClip}/{weaponController.currentAmmo}");
     //}
+
+    private void FlipWeapon(Transform transToRotate, int isRiffle)
+    {
+        if (isRiffle == 0)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transToRotate.position;
+            float rotZ = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+            transToRotate.transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+            if (rotZ < 97 && rotZ > -89)
+            {
+                //Debug.Log("Facing right");
+                transToRotate.transform.Rotate(0f, 0f, transToRotate.transform.rotation.z);
+            }
+            else
+            {
+                //Debug.Log("Facing left");
+                transToRotate.transform.Rotate(180f, 0f, transToRotate.transform.rotation.z);
+            }
+        }
+    }
 
     [PunRPC]
     private void Flip()
